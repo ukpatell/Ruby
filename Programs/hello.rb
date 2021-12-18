dee1177fc0c93a2bffce47a6767aa599a4b21550
Umang <88688048+ukpatell@users.noreply.github.com>
2021-12-17 21:11:20 -0500

    Hello World - print vs puts

---
+++ Programs/hello.rb
@@ -0,0 +1,27 @@
+# Prints the line and adds a new line after
+# System.out.println
+# cout << endl
+
+puts "Hello World from Ruby"
+
+=begin
+    This begin and end block is for multiple comment section.
+    
+    In the next line of code:
+        print => outputs the line in the current line
+=end
+print "Hello World - 2nd Line"
+print "Hello World - 3rd Line"
+
+puts "Hello World - 4th Line"
+puts "Hello World - 5th Line"
+
+=begin
+
+    Output: 
+
+    Hello World from Ruby
+    Hello World - 2nd LineHello World - 3rd LineHello World - 4th Line
+    Hello World - 5th Line
+    
+=end
