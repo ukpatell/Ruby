9d62c5860db5fc0035868d5ba5e58b54fe383ff2
Umang <88688048+ukpatell@users.noreply.github.com>
2021-12-18 17:58:58 -0500

    Added Roman Numerals

---
+++ Programs/romanNumerals.rb
@@ -0,0 +1,24 @@
+# => Name: Umang Patel
+# => Date: December 18, 2021
+# => Description: Map Roman Numerals
+# =>              from 1 to 10
+
+
+number_map = {
+  1=>"I",
+  2=>"II",
+  3=>"III",
+  4=>"IV",
+  5=>"V",
+  6=>"VI",
+  7=>"VII",
+  8=>"VIII",
+  9=>"IX",
+  10=>"X"
+}
+
+puts number_map[5]
+#outputs => V
+
+puts number_map[9] == 'IX'
+#outputs => true
