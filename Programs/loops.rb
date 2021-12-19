ecb59f16016bacd97a88fe8fb6a0c647317bc964
Umang <88688048+ukpatell@users.noreply.github.com>
2021-12-18 19:08:01 -0500

    Added Loops

---
+++ Programs/loops.rb
@@ -0,0 +1,24 @@
+# One of the methods for loop
+i = 5
+
+loop do
+  break if i <= 0
+  puts "Countdown: #{i}"
+  i -= 1
+end
+puts "Blast Off Method 1",""
+
+# Loops : while, until (much better than given above method)
+k = 5
+while k > 0         # built-in break statement
+  puts "Countdown: #{k}"
+  k -= 1
+end
+puts "Blast Off Method 2",""
+
+cart = ['apple','banana','carrot']
+
+until cart.empty?       # loop will continue until the cart become empty
+  first = cart.shift    # remove the first item(shift) from the front and updates it
+  puts first.upcase     # turns upcase
+end
